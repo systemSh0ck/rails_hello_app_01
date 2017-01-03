33bf8ab1f1376bf6646788f5404b37c27fccf73b
Bojan Ivanović <bojan.ivanovic6@gmail.com>
2017-01-03 18:56:30 +0100

    Initializing repository

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,11 @@
+class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+
+  def hello
+    render html: "¡hello planet!"
+  end
+
+  def goodbye
+    render html: "¡fuck off world!"
+  end
+end
